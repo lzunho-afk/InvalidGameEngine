e3eba7dd7de813fc01454eb89942ce4aa6ebc83c
Lucas Zunho <lucaszunho17@gmail.com>
2023-04-22 18:39:54 -0300

    LightRenderer object draw

--- light_renderer.cpp
+++ light_renderer.cpp
@@ -62,32 +62,58 @@ LightRenderer::~LightRenderer()
     
 }
 
+/*
+ * Get models data & draw it
+ */
 void LightRenderer::draw()
 {
-   
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(glm::mat4(1.0), this->position);
+    glUseProgram(this->program);
+
+    // model
+    GLint model_location = glGetUniformLocation(this->program, "model");
+    glUniformMatrix4fv(model_location, 1, GL_FALSE, glm::value_ptr(model));
+
+    // view & project mtxs
+    glm::mat4 view = this->camera->get_view_mtx();
+    GLint view_location = glGetUniformLocation(this->program, "view");
+    glUniformMatrix4fv(view_location, 1, GL_FALSE, glm::value_ptr(view));
+
+    glm::mat4 proj = this->camera->get_proj_mtx();
+    GLint proj_location = glGetUniformLocation(this->program, "projection");
+    glUniformMatrix4fv(proj_location, 1, GL_FALSE, glm::value_ptr(proj));
+
+    // draw
+    glBindVertexArray(this->vao);
+    glDrawElements(GL_TRIANGLES, this->idxs.size(), GL_UNSIGNED_INT, 0);
+
+    // unbinding
+    glBindVertexArray(0);
+    glUseProgram(0);
 }
 
 void LightRenderer::set_position(glm::vec3 _pos)
 {
-    
+    this->position = _pos;
 }
 
 void LightRenderer::set_color(glm::vec3 _color)
 {
-    
+    this->color = _color;
 }
 
 void LightRenderer::set_program(GLuint prog)
 {
-    
+    this->program = prog;
 }
 
 glm::vec3 LightRenderer::get_position()
 {
-    
+    return this->position;
 }
 
 glm::vec3 LightRenderer::get_color()
 {
-    
+    return this->color;
 }
